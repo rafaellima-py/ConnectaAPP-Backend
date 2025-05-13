f2279d6a3a04c346ce196a4e8fddc74d2dd9316d
Dev Cansado <45550842+rafaellima-py@users.noreply.github.com>
2025-05-13 15:16:40 -0300

    correção login

--- routes/auth.py
+++ routes/auth.py
@@ -6,17 +6,18 @@ from security import create_hash, verify_hash
 db = Database()
 auth_router = APIRouter()
 
+
 @auth_router.post("/login")
 async def login(login: UserLogin):
     user = await db.get_user(login.email)
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuario não encontrado")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
     
-    if str(user) == str(login.email) and verify_hash(login.password, user.password):
-        return JSONResponse(content={'user':user.email, 'password':user.password}, status_code=status.HTTP_200_OK)
-    else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Usuário ou senha inválidos")
-    
+    if user["email"] == login.email and verify_hash(login.password, user["password"]):
+        return JSONResponse(content={'detail':'OK', user: user["email"]}, status_code=status.HTTP_200_OK)
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Usuário ou senha inválidos")
+
 @auth_router.post("/register")
 async def register(register: UserRegister):
     user = await db.get_user(register.email)
